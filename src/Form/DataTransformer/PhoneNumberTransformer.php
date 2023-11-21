402d9b1a123d3f53de2192061bdc95c843277475
Zach <37421850+zatcham@users.noreply.github.com>
2023-11-21 20:38:31 +0000

    Phone number now turns into correct format when edited

---
+++ src/Form/DataTransformer/PhoneNumberTransformer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+class PhoneNumberTransformer implements DataTransformerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function transform(mixed $value)
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        return $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reverseTransform(mixed $value)
+    {
+        if (null === $value) {
+            return null;
+        }
+        $phoneNum = str_replace(' ', '', $value); // Remove spaces
+        $phoneNum = '+44' . ltrim($phoneNum, '0'); // Remove leading 0 and replace w +44. TODO : Allow other country codes?
+        return $phoneNum;
+    }
+}
