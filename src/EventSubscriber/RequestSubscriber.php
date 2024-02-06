3b28f4d9905b6339c1922981e9da3792ef778922
Zach <37421850+zatcham@users.noreply.github.com>
2024-02-06 16:01:53 +0000

    Working email notifications on request status update. Work needs to be done on template (as per S2-31), and need to work out what other notifications required. Also, check user settings (S2-32)

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -2,37 +2,90 @@
 
 namespace App\EventSubscriber;
 
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use App\Entity\Request;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 
+#[AsEntityListener(event: Events::postUpdate, method: 'postUpdate', entity: Request::class)]
 class RequestSubscriber implements EventSubscriberInterface
 {
     private $mailer;
 
-    public function _construct(MailerInterface $mailer) {
+    public function __construct(MailerInterface $mailer) {
         $this->mailer = $mailer;
     }
 
-    public function preUpdate(LifecycleEventArgs $args) : void {
-        $entity = $args->getObject();
+    public function postUpdate(Request $request, PostUpdateEventArgs $event) {
 
-        if (!$entity instanceof Request) {
-            return;
+        $entity = $event->getObject();
+
+        $uow = $event->getObjectManager()->getUnitOfWork();
+        $changes = $uow->getEntityChangeSet($entity);
+
+        // This seems to be working
+        if (array_key_exists('status', $changes)) {
+            $email = (new TemplatedEmail())
+                ->from(new Address('app@expenseit.tech', 'ExpenseIt'))
+                ->to('me@zachm.uk')
+                ->subject('An update has been made to your request')
+                ->htmlTemplate('emails/notify_req_update.html.twig')
+                ->context([
+                    'plainPassword' => 'testing',
+                    'companyName' => 'none'
+                ]);
+            $this->mailer->send($email);
         }
-
-        $changeSet = $args->getEntityManager()->getUnitOfWork()->getEntityChangeSet($entity);
-        
     }
+
+
+
+
+//    public function preUpdate(PreUpdateEventArgs $args) : void {
+//        $entity = $args->getObject();
+//
+//        if (!$entity instanceof Request) {
+//            return;
+//        }
+//
+//        $changeSet = $args->getEntityManager()->getUnitOfWork()->getEntityChangeSet($entity);
+//
+//        if (isset($changeSet['status'])) {
+//            $oldStatus = $changeSet['status'][0];
+//            $newStatus = $changeSet['status'][1];
+//
+//            if ($oldStatus != $newStatus) {
+//                $email = (new TemplatedEmail())
+//                    ->from(new Address('app@expenseit.tech', 'ExpenseIt'))
+//                    ->to('me@zachm.uk')
+//                    ->subject('Subscriber')
+//                    ->htmlTemplate('emails/new_user.html.twig')
+//                    ->context([
+//                        'plainPassword' => 'testing',
+//                        'companyName' => 'none'
+//                    ]);
+//                $this->mailer->send($email);
+//            }
+//        }
+//    }
 
     public static function getSubscribedEvents(): array
     {
+
+//        return [
+//            'Doctrine\ORM\Events\PreUpdateEvent' => 'onDoctrine\ORM\Events\PreUpdateEvent',
+//            Events::preUpdate => 'onPreUpdate'
+//        ];
         return [
-            'Doctrine\ORM\Events\PreUpdateEvent' => 'onDoctrine\ORM\Events\PreUpdateEvent',
+            Events::postUpdate => 'postUpdate',
         ];
     }
 }
