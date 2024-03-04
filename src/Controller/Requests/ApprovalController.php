642557833963181a20cbbf2ad9f65390b7d6b123
Zach <37421850+zatcham@users.noreply.github.com>
2024-03-04 19:55:25 +0000

    Bad code to only allow finance admin users to view approvals and only allow approval_rw users to access their department

--- src/Controller/Requests/ApprovalController.php
+++ src/Controller/Requests/ApprovalController.php
@@ -12,7 +12,24 @@ class ApprovalController extends AbstractController
 
     #[Route('/approve/list', name: 'approve_list')]
     public function list(Request $request) : Response {
-        return $this->render('dashboard/approve/list.html.twig');
+
+        $requests = array();
+        $user = $this->getUser();
+        if (in_array('ROLE_FINANCE_ADMIN', $user->getRoles())) {
+            foreach($user->getCompany()->getDepartments() as $department) {
+                foreach($department->getRequests() as $request) {
+                    $requests[] = $request;
+                }
+            }
+        } else {
+            $requests = $this->getUser()->getDepartment()->getRequests();
+        }
+
+        $this->denyAccessUnlessGranted('ROLE_APPROVAL_RW');
+        return $this->render('dashboard/approve/list.html.twig',
+        [
+            'requests' => $requests,
+        ]);
     }
 
 }
