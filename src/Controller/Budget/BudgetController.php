331bb85dfd01e405a46073a1d447a5a48083d441
Zach <37421850+zatcham@users.noreply.github.com>
2024-03-11 04:38:01 +0000

    Initial working example of table showing companies budgets
    - To Dos: Add role-based access (only allow finance admins to see all budgets), fix styling, add more data to entity

---
+++ src/Controller/Budget/BudgetController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Budget;
+
+use App\Entity\Budget;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BudgetController extends AbstractController
+{
+    private $managerRegistry;
+
+    public function __construct(ManagerRegistry $managerRegistry) {
+        $this->managerRegistry = $managerRegistry;
+    }
+
+
+    #[Route('/budget/view', name: 'budget_view')]
+    public function index(): Response
+    {
+        return $this->render('dashboard/budget/view.html.twig', [
+            'controller_name' => 'BudgetController',
+            'budgets' => $this->getAllBudgets()
+        ]);
+    }
+
+
+    private function getAllBudgets() : array {
+        // Working, TODO : Add role-based access
+//        $objects = $this->managerRegistry->getRepository(Budget::class)->findAll();
+        $company = $this->getUser()->getCompany();
+
+        $deptBudgets = [];
+
+        // Iterate all departments in company
+        foreach($company->getDepartments() as $department) {
+            // Get all budgets of department
+            foreach($department->getBudgets() as $budgets) {
+                $b['id'] = $budgets->getId();
+                $b['department_id'] = $budgets->getDepartment()->getId();
+                $deptBudgets[] = $b;
+            }
+        }
+
+        return $deptBudgets;
+    }
+}
